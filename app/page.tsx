9ff4f24af0d0b6248e17f3516595461b8e45a2c3
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-08-15 00:05:10 +0630

    Create Home component with multiple sections

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Hero from "@/components/Hero";
+import Impact from "@/components/Impact";
+import Projects from "@/components/Projects";
+import Stack from "@/components/Stack";
+import Timeline from "@/components/Timeline";
+import Familiar from "@/components/Familiar";
+import Contact from "@/components/Contact";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Impact />
+      <Projects />
+      <Stack />
+      <Timeline />
+      <Familiar />
+      <Contact />
+    </main>
+  );
+}
